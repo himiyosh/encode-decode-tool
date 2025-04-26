f1b360ef119d635277543e52ee71b918f0631096
himiyosh <himiyosh@gmail.com>
2025-04-27 08:38:33 +0900

    update commit

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 5173
-  }
+  base: '/encode-decode-tool/',
+  build: { outDir: 'docs' },
+  plugins: [react()]
 });
